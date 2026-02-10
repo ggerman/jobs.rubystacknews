cfa000503c2742e1b68fee9064d06dd1bba06f90
Giménez Silva Germén Alberto <ggerman@gmail.com>
2026-02-10 15:08:04 -0300

    update styles path

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/styles.css");
 
   eleventyConfig.addCollection("jobs", (collection) =>
-    collection.getFilteredByGlob("src/jobs/*.md").sort((a, b) => {
-      return b.date - a.date;
-    })
+    collection.getFilteredByGlob("src/jobs/*.md").sort((a, b) => b.date - a.date)
   );
 
   return {
